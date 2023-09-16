c4eaa84623982e993acea1ecc0cc67ba771cf3d3
meer habib <meerhabib200@gmail.com>
2023-09-17 01:44:23 +0600

    navigation types added

--- _app/components/routes/root_navigation.tsx
+++ _app/components/routes/root_navigation.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import ExerciseScreen from '../screens/exercise_screen';
-const Stack = createStackNavigator();
+
+export type StackNavigationParamList = {
+  ExerciseScreen: undefined;
+};
+
+const Stack = createStackNavigator<StackNavigationParamList>();
 const RootNavigation = (): JSX.Element => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Exercise">
-        <Stack.Screen name="Exercise" component={ExerciseScreen} />
+      <Stack.Navigator initialRouteName="ExerciseScreen">
+        <Stack.Screen name="ExerciseScreen" component={ExerciseScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
